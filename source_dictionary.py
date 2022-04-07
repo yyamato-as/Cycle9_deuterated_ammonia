1226e8be9cd3de32b5f34e7c98074b13d8887721
yyamato-as <yyamato.as@gmail.com>
2022-04-07 13:09:05 +0900

    Updated files

---
+++ source_dictionary.py
@@ -0,0 +1,8 @@
+source_dict = {
+    "IRAS 16293A": {
+        "v_sys": 3.1,  # in km/s; Jorgensen et al. 2011
+    },
+    "IRAS 16293B": {
+        "v_sys": 2.7,  # in km/s; Jorgensen et al. 2011
+    },
+}
